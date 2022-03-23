6ba990ecf34b0569b79d3fff786774e2490d6b0e
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-24 01:03:12 +0430

    Create book object

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // Create form and book container variables 
 const form = document.getElementById('form');
 const bookKeeper = document.getElementById('book-keeper');
+// Create book object
+const awesomeBook =  function(){
+
+    this.data = {};
+    this.record = [];
