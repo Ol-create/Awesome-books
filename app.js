b80f16f11276ee21510faa163984c322e58f5c0f
Ol-create <oluola96@gmail.com>
2022-03-23 15:04:44 -0600

    Create get items from local storage

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-// Create form and book container variables 
-const form = document.getElementById('form');
-const bookKeeper = document.getElementById('book-keeper');
 // Create book object
 const awesomeBook =  function(){
 
     this.data = {};
     this.record = [];
-    // Creat add book method
+// Creat add book method
   this.addRecord= function(title, author) {
     this.data = { title, author };
     this.record.push(this.data);
     return this.data;
   };
+
   this.local= function() {
-    // Set localStorage
-  localStorage.setItem('books', JSON.stringify(this.record));
-  this.returnInfo();
-};
+      // Set localStorage
+    localStorage.setItem('books', JSON.stringify(this.record));
+    this.returnInfo();
+  };
+
+  this.returnInfo =function() {
+    bookKeeper.innerHTML = '';
+
+    // Get items from local storage
+    this.record = JSON.parse(localStorage.getItem('books'));
+    if (this.record === null) {
+      this.record = [];
+    } else {
+      let count = 0;
+      this.record.forEach((element) => {
+        count += 1;
