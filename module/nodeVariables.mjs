7306b7884648bf1ce77d65d9948e98352e2b767c
Ol-create <oluola96@gmail.com>
2022-03-31 04:48:29 -0600

    feat: add global node variables

---
+++ module/nodeVariables.mjs
@@ -0,0 +1,11 @@
+// Navigation
+const navlist = document.querySelectorAll('.link');
+const section = document.querySelectorAll('.section');
+const bookShelf = document.getElementById('bookList');
+const newBook = document.getElementById('add');
+const book = newBook.querySelectorAll('input');
+const datePlace = document.querySelector('.date');
+
+export {
+  navlist, section, bookShelf, newBook, book, datePlace,
+};
